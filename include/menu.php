d52eb444e1dbeb1dc34773a68ed928aef16e398a
Bryan <magiik.bk@gmail.com>
2021-03-23 15:20:33 +0100

    Test

---
+++ include/menu.php
@@ -0,0 +1,42 @@
+<style>
+ .navbar-brand{
+     margin-left: 50px;
+ }
+ .navbar-nav{
+     margin-left: 600px;
+ }
+
+ .nav-item{
+     margin-left: 40px;
+ }
+.bgBlack{
+    background-color: black;
+}
+
+
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-dark bgBlack">
+  <a class="navbar-brand" href="#">ALLTHINGS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Produit</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Connexion</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<p>test</p>
